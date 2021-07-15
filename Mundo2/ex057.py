188903ed58fda0d47ae14e309c9e5623ce742fc9
VG083 <jnonetto13@gmail.com>
2021-07-15 03:41:21 -0300

    Revert "Mundo 2"
    This reverts commit de698f0313f7f930c25250de5c7b0ada23ce9425.

--- Mundo2/ex057.py
+++
@@ -1,5 +0,0 @@
-# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
-sexo = str(input("Informe seu sexo [M/F]: ")).strip().upper()[0]
-while sexo not in "MFmf":
-    sexo = str(input("Dado inválido. Informe novamente seu sexo: "))
-print("Seu sexo({}) foi registrado.".format(sexo))
--- Mundo2/ex064.py
+++
@@ -1,8 +0,0 @@
-# Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
-n = cont = soma = 0
-n = int(input("Digite um número [999 para parar]: "))
-while n != 999:
-    soma += n
-    cont += 1
-    n = int(input("Digite um número [999 para parar]: "))
-print("Você digitou {} números e a soma entre eles foi de {}".format(cont, soma))
